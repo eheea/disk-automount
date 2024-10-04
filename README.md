5586db151779f35a15ae61ca4c68947afc72abf4
eheea <eheea3333@gmail.com>
2024-10-04 10:02:51 +0300

    first release
    first released file

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # linux-disk-automount-script
 a script to automate disk mounting on linux
+
+#how to use the script
+1- use the "sudo su" command to switch to superuser because the disk mounting needs to be done at a superuser level
+2- copy the id of the disk you want, ID's are on the right ;eg (5575a4c2-f69d-4ff6-8c4d-a8aed3341c6d)
+3- paste the id and hit enter
+4- enter the path of a folder you want the disk to be mounted on; (eg /home/eheea/diskfolder) and hit enter
+5-enter the file system type of the disk; (eg ext4,ntfs,btrfs) and hit enter
+and the disk will be mounted and ready
+check the mounted folder to see if it mounted or not
---
+++ automount.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+lsblk -o NAME,SIZE,TYPE,MOUNTPOINT,UUID
+echo "                                 "
+echo -e "\033[32mplease run this script as root, you can switch to root using sudo su\033[0m"
+echo "         "
+echo "insert disk UUID"
+read -r UUID
+
+echo "                                 "
+
+echo "insert mount point"
+read -r mount
+
+echo "                                 "
+
+
+echo "insert filesystem type"
+read -r fs
+
+if [ ! -d "$mount" ]; then
+mkdir -p "$mount"
+echo "mount point has been created at $mount"
+else  echo "mount point already exists"
+fi
+
+if ! grep -q "$UUID" /etc/fstab; then
+echo UUID="$UUID"  "$mount"  "$fs"  defaults  0  0 >> /etc/fstab
+sudo systemctl daemon-reload
+sudo mount UUID="$UUID"
+if [ $? -eq 0 ]; then
+echo "disk mounted successfully"
+else echo "disk mount failed"
+sed -i '$d' /etc/fstab
+fi
+else echo "disk is already mounted"
+fi
