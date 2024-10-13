484a6cf70900c6f8b647ab9e16bea86629a6638b
eheea <eheea3333@gmail.com>
2024-10-13 07:01:33 +0300

    added unmounting script

--- unmount.sh
+++ unmount.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 lsblk -o NAME,SIZE,TYPE,MOUNTPOINT,UUID
+echo "                   "
 echo "insert the UUID of the disk you want to unmount"
 read -r ID
 umount UUID="$ID"
-sed -i "/$ID/d" /etc/fstab
+if [ $? -eq 0 ]; then
+echo "disk successfully unmounted"
+else echo "the disk is busy. log out or restart the computer and try again"
+fi
+sed -i "/$ID/g" /etc/fstab
+systemctl daemon-reload
